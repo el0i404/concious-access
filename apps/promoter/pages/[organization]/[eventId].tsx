57592e776c459b44636ea1d3ee817e24a3babf79
Eloi <recycled_loops@hotmail.com>
2025-10-17 22:10:45 +0200

    NFT

--- apps/promoter/pages/[organization]/[eventId].tsx
+++ apps/promoter/pages/[organization]/[eventId].tsx
@@ -1,21 +1,36 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { FC, useState } from 'react';
-import { Layout } from '../../components';
-import { Button, Flex, Text } from '@awareness/ui';
-import { useRouter } from 'next/router';
+import { FC, useState } from "react";
+import { Layout } from "../../components";
+import { Button, Flex, Text } from "@awareness/ui";
+import { useRouter } from "next/router";
 import {
   useGetEventWithoutAnswersQuery,
   useSubmitResultMutation,
-} from '@awareness/graphql';
-import { useIsMobile } from '@awareness/hooks';
-import QRCode from 'react-qr-code';
-import Loader from '../../components/loader';
-const optionLabels = ['A', 'B', 'C', 'D']; // maximum number of labels
+} from "@awareness/graphql";
+import { useIsMobile } from "@awareness/hooks";
+import QRCode from "react-qr-code";
+import { useWallet } from "@solana/wallet-adapter-react";
+import Loader from "../../components/loader";
+const optionLabels = ["A", "B", "C", "D"]; // maximum number of labels
+
+import { create, mplCore } from "@metaplex-foundation/mpl-core";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import {
+  createGenericFile,
+  generateSigner,
+  signerIdentity,
+  sol,
+} from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { base58 } from "@metaplex-foundation/umi/serializers";
+
+import path from "path";
 
 function getCurrentDate(date: string) {
   const now = new Date(date);
-  const day = String(now.getDate()).padStart(2, '0'); // Get day and pad with leading zero if necessary
-  const month = String(now.getMonth() + 1).padStart(2, '0'); // Get month (add 1 because months are 0-indexed) and pad with leading zero if necessary
+  const day = String(now.getDate()).padStart(2, "0"); // Get day and pad with leading zero if necessary
+  const month = String(now.getMonth() + 1).padStart(2, "0"); // Get month (add 1 because months are 0-indexed) and pad with leading zero if necessary
   const year = now.getFullYear(); // Get full year
 
   return `${day}.${month}.${year}`;
@@ -23,8 +38,9 @@ function getCurrentDate(date: string) {
 
 const QuizTaking: FC = () => {
   const router = useRouter();
+  const wallet = useWallet();
   const isMobile = useIsMobile();
-  let eventId: any = '';
+  let eventId: any = "";
   if (router.query) {
     eventId = router?.query?.eventId;
   }
@@ -38,8 +54,8 @@ const QuizTaking: FC = () => {
   const [hasPassed, setHasPassed] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isQRCodeScanned, setIsQRCodeScanned] = useState<boolean>(false);
-  const [qrCode, setQrCode] = useState<string>('');
-  const [date, setDate] = useState<string | null | undefined>('');
+  const [qrCode, setQrCode] = useState<string>("");
+  const [date, setDate] = useState<string | null | undefined>("");
 
   /**
    * getEventDetails & Questions
@@ -53,7 +69,7 @@ const QuizTaking: FC = () => {
     variables: { getEventWithoutAnswersId: eventId },
   });
 
-  console.log('eventData', eventData);
+  console.log("eventData", eventData);
   const currentQues =
     eventData?.getEventWithoutAnswers?.guide_lines?.[currentQuestion];
 
@@ -67,6 +83,68 @@ const QuizTaking: FC = () => {
     ) {
       setCurrentQuestion(currentQuestion + 1);
     }
+  };
+
+  const createNft = async () => {
+    const umi = createUmi("https://api.devnet.solana.com")
+      .use(mplCore())
+      // Register Wallet Adapter to Umi
+      .use(
+        irysUploader({
+          // mainnet address: "https://node1.irys.xyz"
+          // devnet address: "https://devnet.irys.xyz"
+          address: "https://devnet.irys.xyz",
+        })
+      );
+
+    const signer = generateSigner(umi);
+
+    umi.use(signerIdentity(signer));
+    console.log("Airdropping 1 SOL to identity");
+    await umi.rpc.airdrop(umi.identity.publicKey, sol(1));
+
+    const metadata = {
+      name: "My NFT",
+      description: "This is an NFT on Solana",
+      image:
+        "https://awareness-promoter.vercel.app/hotmail/671bd1f2a166df689f0adb4c",
+      external_url: "https://example.com",
+      attributes: [
+        {
+          trait_type: "trait1",
+          value: "value1",
+        },
+        {
+          trait_type: "trait2",
+          value: "value2",
+        },
+      ],
+      properties: {
+        files: [
+          {
+            uri: "https://awareness-promoter.vercel.app/hotmail/671bd1f2a166df689f0adb4c",
+            type: "image/jpeg",
+          },
+        ],
+        category: "image",
+      },
+    };
+
+    const metadataUri = await umi.uploader.uploadJson(metadata).catch((err) => {
+      throw new Error(err);
+    });
+
+    const asset = generateSigner(umi);
+
+    const tx = await create(umi, {
+      asset,
+      name: "My NFT",
+      uri: metadataUri,
+    }).sendAndConfirm(umi);
+
+    const signature = base58.deserialize(tx.signature)[0];
+
+    console.log("signature", signature);
   };
 
   /**
@@ -85,12 +163,12 @@ const QuizTaking: FC = () => {
     // Combine user answers with question IDs
     const mcq_question_list =
       eventData?.getEventWithoutAnswers?.guide_lines?.filter(
-        (guideLine) => guideLine?.type !== 'short_text'
+        (guideLine) => guideLine?.type !== "short_text"
       );
 
     const quizData = (mcq_question_list ?? []).map((guideLine) => ({
-      id: guideLine?.id ?? '',
-      correct_answer: userAnswers[guideLine?.id ?? ''] || '', // default to an empty string if not answered
+      id: guideLine?.id ?? "",
+      correct_answer: userAnswers[guideLine?.id ?? ""] || "", // default to an empty string if not answered
     }));
 
     if (eventData?.getEventWithoutAnswers.id && quizData.length > 0) {
@@ -102,10 +180,11 @@ const QuizTaking: FC = () => {
             eventResult: quizData,
           },
         });
+        createNft();
         if (response.data?.submitResult?.is_passed) {
           setHasPassed(response.data?.submitResult?.is_passed);
           setIsQRCodeScanned(response.data?.submitResult?.is_qr_code_scanned);
-          setQrCode(response.data?.submitResult?.qr_code_number ?? '');
+          setQrCode(response.data?.submitResult?.qr_code_number ?? "");
           setDate(response.data?.submitResult?.createdAt);
         }
       } catch (error: any) {
@@ -123,26 +202,26 @@ const QuizTaking: FC = () => {
         <Loader />
       ) : !currentQues || isError || getEventError ? (
         <Flex
-          flexDirection={'column'}
-          justifyContent={'flex-end'}
+          flexDirection={"column"}
+          justifyContent={"flex-end"}
           fontFamily="Urbanist"
         >
           {getEventError || !currentQues ? (
             <>
-              <Text fontSize={'32px'} marginBottom={24} fontWeight={700}>
+              <Text fontSize={"32px"} marginBottom={24} fontWeight={700}>
                 It’s empty quiz, contact support for further assistance
               </Text>
             </>
           ) : (
             <>
-              <Text fontSize={'56px'} fontWeight={800}>
+              <Text fontSize={"56px"} fontWeight={800}>
                 Not Quite
               </Text>
               <Text marginBottom={24} fontSize={2} fontWeight={400}>
                 You didn’t pass. Take the quiz again.
               </Text>
               <Button
-                width={'130px'}
+                width={"130px"}
                 size="small"
                 onClick={() => {
                   router.push(window.location.href);
@@ -152,7 +231,7 @@ const QuizTaking: FC = () => {
                   setShowLogo(false);
                 }}
               >
-                <Text color={'black'} fontSize={2}>
+                <Text color={"black"} fontSize={2}>
                   Restart Quiz
                 </Text>
               </Button>
@@ -173,13 +252,13 @@ const QuizTaking: FC = () => {
           >
             <div
               style={{
-                padding: '4em',
-                paddingBottom: '1em',
+                padding: "4em",
+                paddingBottom: "1em",
               }}
             >
               <QRCode
                 size={256}
-                style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
+                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
                 value={qrCode}
                 viewBox={`0 0 256 256`}
               />
@@ -201,7 +280,7 @@ const QuizTaking: FC = () => {
               alignItems="start"
               justifyContent="center"
               flexDirection="column"
-              backgroundColor={'#E37A58'}
+              backgroundColor={"#E37A58"}
               padding="16px"
             >
               <Text fontSize="28px" fontWeight={600} color="#000">
@@ -238,14 +317,14 @@ const QuizTaking: FC = () => {
           fontFamily="Urbanist"
           flexDirection="column"
           width="100%"
-          position={isMobile ? 'relative' : 'unset'}
+          position={isMobile ? "relative" : "unset"}
         >
           {/* Current Question based on it's type */}
           <div
             key={currentQues?.id}
-            style={{ paddingTop: '20px', fontFamily: 'Urbanist' }}
+            style={{ paddingTop: "20px", fontFamily: "Urbanist" }}
           >
-            {currentQues && currentQues?.type === 'short_text' ? (
+            {currentQues && currentQues?.type === "short_text" ? (
               <>
                 {/* Short text */}
                 <Text fontSize={6} fontWeight={700} lineHeight="40px">
@@ -267,42 +346,42 @@ const QuizTaking: FC = () => {
                   <div
                     key={index}
                     style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      border: '2px solid white',
+                      display: "flex",
+                      flexDirection: "column",
+                      border: "2px solid white",
                       padding: 16,
                       marginTop: 32,
-                      borderRadius: '8px',
+                      borderRadius: "8px",
                       backgroundColor:
                         userAnswers[currentQues?.id] === question?.value
-                          ? '#FFFFFF'
-                          : '#3C3C3C',
-                      boxShadow: '8px 8px black, 8px 8px 0px 2px #FFFFFF',
-                      cursor: 'pointer', // Add cursor style to indicate click ability
+                          ? "#FFFFFF"
+                          : "#3C3C3C",
+                      boxShadow: "8px 8px black, 8px 8px 0px 2px #FFFFFF",
+                      cursor: "pointer", // Add cursor style to indicate click ability
                     }}
                     onClick={() => {
                       setUserAnswers({
                         ...userAnswers,
-                        [currentQues?.id]: question?.value ?? '',
+                        [currentQues?.id]: question?.value ?? "",
                       });
                     }}
                   >
                     <Text
                       color={
                         userAnswers[currentQues?.id] === question?.value
-                          ? 'black'
-                          : '#B5B5B5'
+                          ? "black"
+                          : "#B5B5B5"
                       }
                       fontSize={1}
                       fontWeight={700}
                     >
-                      {optionLabels[index]} {'. '}
+                      {optionLabels[index]} {". "}
                     </Text>
                     <Text
                       color={
                         userAnswers[currentQues?.id] === question?.value
-                          ? 'black'
-                          : 'white'
+                          ? "black"
+                          : "white"
                       }
                       fontSize={3}
                     >
@@ -318,8 +397,8 @@ const QuizTaking: FC = () => {
           <div
             className={
               isMobile
-                ? 'button-mobile-screen'
-                : 'button-positions-normal-screen'
+                ? "button-mobile-screen"
+                : "button-positions-normal-screen"
             }
           >
             <div>
